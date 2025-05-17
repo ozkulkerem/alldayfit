1264e9f3078cbf0d3c52527d754ea063b931677f
keremcan.ozkul <keremcan.ozkul@gmail.com>
2025-05-17 23:43:46 +0300

    up

--- next.config.mjs
+++ next.config.mjs
@@ -5,15 +5,15 @@
 
 // next.config.mjs
 const isProd = process.env.NODE_ENV === "production";
-const repo = "alldayfit"; // ↲ GitHub repo adın
+const repo = "alldayfit";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // statik HTML üretir :contentReference[oaicite:0]{index=0}
+  output: "export",
   basePath: isProd ? `/${repo}` : "",
   assetPrefix: isProd ? `/${repo}/` : "",
-  images: { unoptimized: true }, // Image optimizer'ı kapat
-  trailingSlash: true, // /about/ gibi path’ler için 404 önler
+  images: { unoptimized: true },
+  trailingSlash: true,
 };
 
 export default nextConfig;
